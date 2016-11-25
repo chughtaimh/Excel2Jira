b588eb33f0385c6dfe7ce158a968e53e0a9f4afb
Moe Chughtai <chughtai.mh@gmail.com>
2016-11-25 14:16:06 -0500

    Added support for py2app for mac

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import platform
 from distutils.core import setup
-import py2exe
 
-setup(console=['excel2jira.py'])
+if platform.system().lower() == 'darwin':
+	import py2app
+	setup(app=["excel2jira.py"], setup_requires=["py2app"])
+
+else:
+	import py2exe
+	setup(console=['excel2jira.py'])
+
