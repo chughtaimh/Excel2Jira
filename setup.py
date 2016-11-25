bf7a836a5de2935dc352817098cb42d0dfaddb12
Moe Chughtai <chughtai.mh@gmail.com>
2016-11-25 14:35:18 -0500

    pushed logic into main function

--- setup.py
+++ setup.py
@@ -3,17 +3,22 @@ import platform
 from distutils.core import setup
 
 
-OPTIONS = {
-    'iconfile':'atlassian.icns',
-    'plist': {'CFBundleShortVersionString':'0.1.0',}
-}
+def main():
+	OPTIONS = {
+	    'iconfile':'atlassian.icns',
+	    'plist': {'CFBundleShortVersionString':'0.1.0',}
+	}
 
-if platform.system().lower() == 'darwin':
-	import py2app
-	setup(
-		app=["excel2jira.py"], 
-		setup_requires=["py2app"],
-		options={'py2app': OPTIONS})
-else:
-	import py2exe
-	setup(console=['excel2jira.py'])
+	if platform.system().lower() == 'darwin':
+		import py2app
+		setup(
+			app=["excel2jira.py"], 
+			setup_requires=["py2app"],
+			options={'py2app': OPTIONS})
+	else:
+		import py2exe
+		setup(console=['excel2jira.py'])
+
+
+if __name__ == '__main__':
+	main()
