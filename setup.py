54bca36cbd7d872ecd94d663c26e78a8cff24087
Moe Chughtai <chughtai.mh@gmail.com>
2016-11-25 14:33:13 -0500

    Added ability to set icon

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@ import platform
 
 from distutils.core import setup
 
+
+OPTIONS = {
+    'iconfile':'atlassian.icns',
+    'plist': {'CFBundleShortVersionString':'0.1.0',}
+}
+
 if platform.system().lower() == 'darwin':
 	import py2app
-	setup(app=["excel2jira.py"], setup_requires=["py2app"])
-
+	setup(
+		app=["excel2jira.py"], 
+		setup_requires=["py2app"],
+		options={'py2app': OPTIONS})
 else:
 	import py2exe
 	setup(console=['excel2jira.py'])
-
